7c24cdfc8f425538dbece9c2f6564a1180de6e5c
Casraw <casraw@protonmail.com>
2024-12-28 12:44:09 +0100

    update

--- stratum/job.h
+++ stratum/job.h
@@ -30,9 +30,6 @@ struct YAAMP_JOB_TEMPLATE
 	char extradata_hex[512];
 	char extradata_be[512];
 
-	char accumulatorcheckpoint_hex[512];
-	char accumulatorcheckpoint_be[512];
-
 	// todo: can use extra field
 	char claim_hex[128];
 	char claim_be[128];
@@ -54,6 +51,9 @@ struct YAAMP_JOB_TEMPLATE
 
 	char coinb1[4*1024];
 	char coinb2[4*1024];
+	char coinforsubmitb1[4*1024];
+	char coinforsubmitb2[4*1024];
+	bool isbitcash;
 
 	char header[256];
 
@@ -64,6 +64,9 @@ struct YAAMP_JOB_TEMPLATE
 
 	int auxs_size;
 	YAAMP_COIND_AUX *auxs[MAX_AUXS];
+	
+	bool needpriceinfo;
+	char priceinfo[1024];	
 };
 
 #define YAAMP_JOB_MAXSUBIDS		200
