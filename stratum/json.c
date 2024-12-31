ddab036ff8f9f78b47fc86846476c34fcec06a59
Casraw <casraw@protonmail.com>
2024-12-31 05:54:20 +0100

    remove unused (working state but only without mining.configure)

--- stratum/json.c
+++
@@ -1,8 +0,0 @@
-#include "json.h"
-
-// Define the constant placeholder for invalid JSON values
-#ifdef __cplusplus
-const struct _json_value json_value_none; /* Zero-initialized by the constructor */
-#else
-const struct _json_value json_value_none = { 0 }; /* Explicitly zero-initialized */
-#endif
